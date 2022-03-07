55e7c797e5cf1cc003abb9df0cd301c7a66ffc83
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-07 17:53:35 -0300

    Adicionando formatador de JS

--- 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/5-switch-e-case/script.js
+++ 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/5-switch-e-case/script.js
@@ -1,16 +1,15 @@
 let situacao = "lista";
 
 switch (situacao) {
-    case "aprovada":
-        console.log("aprovada");
-        break;
-    case "lista":
-        console.log("lista");
-        break;
-    case "reprovada":
-        console.log("reprovada");
-        break;
-    default:
-        console.log("não se aplica");
-        break;
+  case "aprovada":
+    console.log("aprovada");
+    break;
+  case "lista":
+    console.log("lista");
+    break;
+  case "reprovada":
+    console.log("reprovada");
+    break;
+  default:
+    console.log("não se aplica");
 }
