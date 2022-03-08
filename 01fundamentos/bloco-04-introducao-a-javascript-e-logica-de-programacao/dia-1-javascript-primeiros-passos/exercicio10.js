bbcbc6b43a52b1ab765e13b0e9f40c653e1df022
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-07 22:58:52 -0200

    Finalizando exercícios

--- 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
+++ 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -1,12 +1,12 @@
 //10.Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 const valorCusto = 200;
-let totalCusto = valorCusto * 120 / 100;
+let totalCusto = valorCusto * (120 / 100);
 const valorVenda = 250;
 let totalVendas = 1000;
 
-if(valorCusto < 0 || valorVenda < 0) {
-    console.log("Os valores informados não podem ser negativos.");
+if (valorCusto < 0 || valorVenda < 0) {
+  console.log("Os valores informados não podem ser negativos.");
 } else {
-    let lucroVendas = (valorVenda - totalCusto) * totalVendas;
-    console.log("O lucro total foi de " + lucroVendas + "reais.")
+  let lucroVendas = (valorVenda - totalCusto) * totalVendas;
+  console.log("O lucro total foi de ", lucroVendas);
 }
