92816d387c69a555358b871e6e25392b42dcbfc1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-10 16:40:54 -0300

    Exercícios 4, 6 e 7 da parte II finalizados

--- 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte2/exercicios.js
+++ 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte2/exercicios.js
@@ -31,16 +31,74 @@ indexOfMajorValue([5, 12, 29, 16]);
 
 //3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 function indexOfMinorValue(array) {
-    let min = array[0];
-  
-    for (let value of array) {
-      if (value < min) {
-        min = value;
-      }
+  let min = array[0];
+
+  for (let value of array) {
+    if (value < min) {
+      min = value;
     }
-  
-    //return array.indexOf(min);
-    console.log(array.indexOf(min));
   }
-  
-  indexOfMinorValue([5, 12, 29, 16]);
+
+  //return array.indexOf(min);
+  console.log(array.indexOf(min));
+}
+
+indexOfMinorValue([5, 12, 29, 16]);
+
+/*4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+  Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']*/
+function printLongestString(stringsArray) {
+  let longestStringNumber = 0;
+  let longestString = "";
+
+  for (word of stringsArray) {
+    if (word.length > longestStringNumber) {
+      longestStringNumber = word.length;
+      longestString = word;
+    }
+  }
+
+  //return longestString
+  console.log(longestString);
+}
+
+printLongestString(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]);
+
+/*5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+Array de teste: [2, 3, 2, 5, 8, 2, 3]*/
+function printModeOfIntegersArray(integersArray) {
+    let mode = math.mode(integersArray);
+
+    //return mode;
+    console.log(mode);
+}
+
+//printModeOfIntegersArray([2, 3, 2, 5, 8, 2, 3]);
+
+//6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+function sumNaturalNumbersOfOneUntilN(naturalNumber) {
+  let sum = 0;
+
+  for (let counter = 1; counter <= naturalNumber; counter += 1) {
+    sum += counter;
+  }
+
+  //return sum;
+  console.log(sum);
+}
+
+sumNaturalNumbersOfOneUntilN(5);
+
+//7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word.
+function verifyMatchingSufixes (word, sufix) {
+    let sufixLength = word.length - sufix.length;
+    let wordSufix = word.substr(sufixLength);
+
+    if(wordSufix === sufix) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+verifyMatchingSufixes ('trybe', 'be');
