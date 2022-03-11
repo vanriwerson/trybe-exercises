fefe1d0ac71ca15fcb909afaa202908e2cc7de10
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-11 10:28:56 -0200

    Refatoração 4.1 de 1 a 5 concluída

--- 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/3-funcoes/refatoracao4.1.js
+++ 01fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/3-funcoes/refatoracao4.1.js
@@ -35,35 +35,35 @@ console.log(basicArithmeticOperations(8, 4, "%"));
 //2.Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 //3.Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 function findMajorOrMinorNumber(arrayOfNumbers, operator) {
-    let searchingNumber = arrayOfNumbers[0]
+  let searchingNumber = arrayOfNumbers[0];
 
-    for (num = 0; num <= arrayOfNumbers.length; num += 1) {
-        if(operator === '>') {
-            if(arrayOfNumbers[num] > searchingNumber) {
-                searchingNumber = arrayOfNumbers[num];
-            }
-        } else {
-            if(arrayOfNumbers[num] < searchingNumber) {
-                searchingNumber = arrayOfNumbers[num];
-            }
-        }
+  for (num = 0; num <= arrayOfNumbers.length; num += 1) {
+    if (operator === ">") {
+      if (arrayOfNumbers[num] > searchingNumber) {
+        searchingNumber = arrayOfNumbers[num];
+      }
+    } else {
+      if (arrayOfNumbers[num] < searchingNumber) {
+        searchingNumber = arrayOfNumbers[num];
+      }
     }
+  }
 
-    return `O número procurado é ${searchingNumber}`;
+  return `O número procurado é ${searchingNumber}`;
 }
 
-console.log(findMajorOrMinorNumber([3, 16, 29, 7], '>'));
-console.log(findMajorOrMinorNumber([3, 16, 29, 7], '<'));
+console.log(findMajorOrMinorNumber([3, 16, 29, 7], ">"));
+console.log(findMajorOrMinorNumber([3, 16, 29, 7], "<"));
 
 //4.Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-function returnNumberSign (number) {
-    if (number > 0) {
-         return "O número informado é positivo";
-      } else if (number < 0) {
-        return "O número informado é negativo";
-      } else {
-        return "O número informado é zero";
-      }
+function returnNumberSign(number) {
+  if (number > 0) {
+    return "O número informado é positivo";
+  } else if (number < 0) {
+    return "O número informado é negativo";
+  } else {
+    return "O número informado é zero";
+  }
 }
 
 console.log(returnNumberSign(7));
@@ -71,14 +71,16 @@ console.log(returnNumberSign(0));
 console.log(returnNumberSign(-3));
 
 //5.Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-const anguloA = 45;
-const anguloB = -40;
-const anguloC = 90;
-
-if (anguloA + anguloB + anguloC === 180) {
-  console.log(true);
-} else if (anguloA <= 0 || anguloB <= 0 || anguloC <= 0) {
-  console.log("Algum dos ângulos informados é inválido!");
-} else {
-  console.log(false);
+function checkIfThisAnglesMakeATriangle(angleA, angleB, angleC) {
+  if (angleA + angleB + angleC === 180) {
+    return "Sim. Esses ângulos formam um triângulo.";
+  } else if (angleA <= 0 || angleB <= 0 || angleC <= 0) {
+    return "Algum dos ângulos informados é inválido!";
+  } else {
+    return "Não. Esses ângulos não formam um triângulo.";
+  }
 }
+
+console.log(checkIfThisAnglesMakeATriangle(40, 50, 60));
+console.log(checkIfThisAnglesMakeATriangle(-70, 50, 60));
+console.log(checkIfThisAnglesMakeATriangle(70, 50, 60));
