0507d9eadf235576c78b2189698b731a7d23fca6
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-15 17:56:19 -0300

    Dando ênfase aos feriados

--- 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
+++ 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
@@ -24,41 +24,10 @@ createDaysOfTheWeek();
 //EXERCÍCIO 01.
 function createDaysOfTheMonth() {
   const dezDaysList = [
-    29,
-    30,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
   ];
-  const hollidaysList = [24, 25, 31];
+  const holidaysList = [24, 25, 31];
   const fridayDaysList = [4, 11, 18, 25];
   const daysList = document.getElementById("days");
 
@@ -67,7 +36,7 @@ function createDaysOfTheMonth() {
     day.setAttribute("class", "day");
     day.innerText = dezDaysList[i];
 
-    for (let holliday of hollidaysList) {
+    for (let holliday of holidaysList) {
       if (holliday === dezDaysList[i]) {
         day.classList.add("holiday");
       }
@@ -89,7 +58,7 @@ createDaysOfTheMonth();
 function createButton(label) {
   const buttonsContainer = document.querySelector(".buttons-container");
   let newButton = document.createElement("button");
-  
+
   newButton.setAttribute("id", "btn-holiday");
   newButton.innerHTML = label;
 
@@ -97,3 +66,31 @@ function createButton(label) {
 }
 
 createButton("Feriados");
+
+//EXERCÍCIO 03.
+let holidayEmphasis = false;
+function changeHollidaysBG(){
+    let daysList = document.querySelectorAll(".day")
+    
+    for (i = 0; i < daysList.length; i += 1) {
+        if (daysList[i].classList.contains('holiday')) {
+            if (holidayEmphasis === false) {
+                daysList[i].style.backgroundColor = 'green';
+                daysList[i].style.color = '#eee';
+            } else {
+                daysList[i].style.backgroundColor = '#eee';
+                daysList[i].style.color = '#777';
+            }
+        }
+    }
+
+    if (holidayEmphasis === false) {
+        holidayEmphasis = true;
+    } else {
+        holidayEmphasis = false;
+    }
+}
+
+const holidayBtn = document.getElementById('btn-holiday');
+holidayBtn.addEventListener('click', changeHollidaysBG);
+//background-color: '#036b52';
