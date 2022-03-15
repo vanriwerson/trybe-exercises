86f248f49432102ffe4f0a42540510d45d776ac9
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-15 10:26:07 -0200

    Primeiro eventListener

--- 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
+++ 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
@@ -4,13 +4,11 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
-
-// 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+firstLi.addEventListener('click', firstLi.classList.toggle('tech'));
+secondLi.addEventListener('click', secondLi.classList.toggle('tech'));
+thirdLi.addEventListener('click', thirdLi.classList.toggle('tech'));
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
