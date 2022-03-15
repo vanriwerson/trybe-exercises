7c54c13523d78131802aae8b6e2718f74cb49793
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-15 16:30:21 -0300

    Criando botão feriados

--- 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
+++ 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
@@ -21,13 +21,42 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
-
 //EXERCÍCIO 01.
 function createDaysOfTheMonth() {
   const dezDaysList = [
-    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    29,
+    30,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31
   ];
   const hollidaysList = [24, 25, 31];
   const fridayDaysList = [4, 11, 18, 25];
@@ -55,3 +84,16 @@ function createDaysOfTheMonth() {
 }
 
 createDaysOfTheMonth();
+
+//EXERCÍCIO 02.
+function createButton(label) {
+  const buttonsContainer = document.querySelector(".buttons-container");
+  let newButton = document.createElement("button");
+  
+  newButton.setAttribute("id", "btn-holiday");
+  newButton.innerHTML = label;
+
+  buttonsContainer.appendChild(newButton);
+}
+
+createButton("Feriados");
