3b8e4b631fb7d97c5aaca64af78c7f21a17cd342
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-15 13:07:05 -0300

    Redirecionando para meu portfólio

--- 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
+++ 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
@@ -35,7 +35,11 @@ input.addEventListener('input', atualizeTechText);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+function redirectToMyPortfolio() {
+  window.location.href = "https://vanriwerson.github.io/";
+}
 
+thirdLi.addEventListener('dblclick', redirectToMyPortfolio)
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
