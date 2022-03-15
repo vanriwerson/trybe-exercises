5b8163e0000299566e8d78c5828be74290c7da18
Bruno Riwerson Silva <riwerson@gmail.com>
2022-03-15 13:22:00 -0300

    Prática do conteúdo finalizada

--- 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
+++ 01fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/conteudo/main.js
@@ -40,12 +40,21 @@ function redirectToMyPortfolio() {
 }
 
 thirdLi.addEventListener('dblclick', redirectToMyPortfolio)
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function changeColor(event) {
+  event.target.style.color = '#006dfb';
+}
+function resetColor(event) {
+  event.target.style.color = 'white';
+}
+
+myWebpage.addEventListener('mouseenter', changeColor);
+myWebpage.addEventListener('mouseleave', resetColor)
+
 
 // Segue abaixo um exemplo do uso de event.target:
-
-
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
