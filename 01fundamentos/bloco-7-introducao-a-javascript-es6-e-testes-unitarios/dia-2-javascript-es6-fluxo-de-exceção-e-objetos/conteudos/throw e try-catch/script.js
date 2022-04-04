6b466bb6b54a064ff4ba1b2b76b92031a5ff40de
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-04 11:51:24 -0300

    Conteúdo até object keys

---
+++ 01fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-exceção-e-objetos/conteudos/throw e try-catch/script.js
@@ -0,0 +1,17 @@
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+console.log(sum(2, 3));
+console.log(sum(2, '3'));
