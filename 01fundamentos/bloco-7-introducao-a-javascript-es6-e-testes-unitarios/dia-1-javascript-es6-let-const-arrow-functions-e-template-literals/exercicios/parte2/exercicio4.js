88676601d3c12d2cddf5325e6f323daf6571bdc9
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-04 11:24:58 -0300

    Exercício 4 concluído

--- 01fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/parte2/exercicio4.js
+++ 01fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/parte2/exercicio4.js
@@ -1,13 +1,29 @@
-// Crie um código JavaScript com a seguinte especificação:
-// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
-// Exemplo:
-// String determinada: "Tryber x aqui!"
-// Parâmetro: "Bebeto"
-// Retorno: "Tryber Bebeto aqui!"
 // Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+const skills = ['Git', 'HTML', 'CSS', 'Javascript', 'Didática'];
+
+// Crie um código JavaScript com a seguinte especificação:
+
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string.
+const greetings = (name) => {
+  phrase = 'Tryber x aqui!';
+  const newPhrase = phrase.replace('x', name);
+  return newPhrase
+  //`Tryber ${name} aqui!`;
+}
+
+console.log(greetings('Bruno'));
 
 
 // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
-// Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
-// JavaScript;
-// HTML; ... #goTrybe".
+const joinGreetingsAndSkills = (initialPhrase) => {
+  skills.sort()
+  const fullGreetings = `${initialPhrase} Minhas cinco principais habilidades são:
+  ${skills[0]};
+  ${skills[1]};
+  ${skills[2]};
+  ${skills[3]};
+  ${skills[4]}. #goTrybe`;
+  return fullGreetings;
+}
+
+console.log(joinGreetingsAndSkills(greetings('Bruno')));
