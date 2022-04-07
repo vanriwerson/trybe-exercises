ca8a49d1c000e3a50f43f449398560271bd431c8
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-07 20:06:40 -0300

    Testes de conversão de vogais concluídos

--- 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/encode.spec.js
+++ 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/encode.spec.js
@@ -1,3 +1,28 @@
 const encode = require('./encode');
-const decode = require('./encode');
+const decode = require('./decode');
 
+describe('Verificar se encode e decode são funções', () => {
+  it('Verificar se o typeof de encode e decode retorna "function"', () => {
+    expect(typeof encode).toBe('function');
+    expect(typeof decode).toBe('function');
+  })
+});
+
+describe('Verificar se na função encode as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+  it('Verificar se a vogal "a" é convertida em 1', () => {expect(encode('a')).toBe('1')});
+  it('Verificar se a vogal "e" é convertida em 2', () => {expect(encode('e')).toBe('2')});
+  it('Verificar se a vogal "i" é convertida em 3', () => {expect(encode('i')).toBe('3')});
+  it('Verificar se a vogal "o" é convertida em 4', () => {expect(encode('o')).toBe('4')});
+  it('Verificar se a vogal "u" é convertida em 5', () => {expect(encode('u')).toBe('5')});
+});
+
+describe('Verificar se na função decode os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+  it('Verificar se o número 1 é convertido na vogal "a"', () => {expect(decode('1')).toBe('a')});
+  it('Verificar se o número 2 é convertido na vogal "e"', () => {expect(decode('2')).toBe('e')});
+  it('Verificar se o número 3 é convertido na vogal "i"', () => {expect(decode('3')).toBe('i')});
+  it('Verificar se o número 4 é convertido na vogal "o"', () => {expect(decode('4')).toBe('o')});
+  it('Verificar se o número 5 é convertido na vogal "u"', () => {expect(decode('5')).toBe('u')});
+});
+
+// Teste se as demais letras/números não são convertidos para cada caso;
+// Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro.
