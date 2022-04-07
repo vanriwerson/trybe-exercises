fec8217838340a9b03058fb40e718cb7d9302139
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-07 14:57:44 -0300

    Testes parte 1 concluídos

--- 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.spec.js
+++ 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.spec.js
@@ -1,2 +1,20 @@
 const sum = require('./sum');
 
+describe('1 -Verificar se a função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+  it('Verificar se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+  it('Verificar se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('Verificar se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+  it('Verificar se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError(/parameters must be numbers/);
+  });
+});
