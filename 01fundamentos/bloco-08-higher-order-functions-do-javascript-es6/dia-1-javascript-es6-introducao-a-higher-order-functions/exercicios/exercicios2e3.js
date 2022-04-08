2a9bb4b75080bf3fabe3647d2e659e89df288fad
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-08 12:47:45 -0300

    Refatoração exercício 3

--- 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicios2e3.js
+++ 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicios2e3.js
@@ -15,6 +15,8 @@ console.log(drawResult(2, checkPrizeDraw));
 
 // 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
 
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
@@ -27,6 +29,10 @@ const countRightAnswers = (template, answers) => {
   for (let i in template) {
     if (template[i] === answers[i]) {
       count += 1;
+    } else if (answers[i] === 'N.A') {
+      count += 0;
+    } else {
+      count -= 0.5;
     }
   }
   return count;
