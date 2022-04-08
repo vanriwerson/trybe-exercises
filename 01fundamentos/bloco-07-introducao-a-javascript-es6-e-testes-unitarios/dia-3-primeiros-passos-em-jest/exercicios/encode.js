8b7d79ccc7dca17ccb6a99919254ca945e221be8
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-08 08:19:41 -0300

    techList concluído

--- 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/encode.js
+++ 01fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/encode.js
@@ -5,4 +5,14 @@ const encode = (string) => {
   return encoded;
 };
 
-module.exports = encode;
+const decode = (string) => {
+  string = string.toLowerCase();
+
+  const decoded = string.replaceAll('1', 'a').replaceAll('2', 'e').replaceAll('3', 'i').replaceAll('4', 'o').replaceAll('5', 'u');
+  return decoded;
+};
+
+module.exports = {
+  encode,
+  decode,
+};
