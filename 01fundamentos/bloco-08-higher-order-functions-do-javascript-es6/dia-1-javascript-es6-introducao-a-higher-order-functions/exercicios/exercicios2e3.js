2f420ccc97740f83c5101877ba3e8661058f6af2
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-08 12:39:23 -0300

    Exercício 3 concluído

--- 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicios2e3.js
+++ 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicios2e3.js
@@ -14,3 +14,22 @@ const drawResult = (bet, check) => {
 console.log(drawResult(2, checkPrizeDraw));
 
 // 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const totalRightAnswers = (template, answers, check) => {
+  return `Sua pontuação foi: ${check(template, answers)}`;
+};
+
+const countRightAnswers = (template, answers) => {
+  let count = 0;
+  for (let i in template) {
+    if (template[i] === answers[i]) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+console.log(totalRightAnswers(RIGHT_ANSWERS, STUDENT_ANSWERS, countRightAnswers));
