d0852004522bbe71adad91c649fe7a7f4296c434
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-09 08:03:19 -0300

    Exercício 6 concluído

--- 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercise6.js
+++ 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercise6.js
@@ -4,5 +4,7 @@ const books = require('./template');
 const expectedResult = true;
 
 function someBookWasReleaseOnThe80s() {
-  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
 }
+
+console.log(someBookWasReleaseOnThe80s());
