920888cb442a142aac2d841f96398b7c0e45f07d
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-10 07:59:11 -0300

    Exercício 5 concluído

--- 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicios/exercicio5.js
+++ 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicios/exercicio5.js
@@ -70,5 +70,6 @@ const expectedResult = [
 
 // Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 function fantasyOrScienceFictionAuthors() {
-  
+  return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia').map((book) => book.author.name).sort();
 }
+console.log(fantasyOrScienceFictionAuthors());
