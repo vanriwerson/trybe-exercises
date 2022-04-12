74fbf752163e55a9e8992e8698505aa83cfb16ca
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-12 12:43:00 -0300

    Exercício 4 concluído

---
+++ 01fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios/exercicio4.js
@@ -0,0 +1,31 @@
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20. Dica: use object destructuring .
+const filterPeople = () => people.filter(({ bornIn, nationality }) => nationality === 'Australian' && bornIn >= 1901 && bornIn <= 2000);
+console.log(filterPeople());
