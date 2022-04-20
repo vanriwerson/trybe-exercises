4c739d3153548dee376902cfdf9fc907d5dad174
Bruno Riwerson Silva <riwerson@gmail.com>
2022-04-20 08:44:11 -0300

    Conteúdo API concluído

---
+++ 01fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/conteudos/APIs/script.js
@@ -0,0 +1,48 @@
+const jsonInfo = `{
+  "muitasEmpresasUsam": [
+    "Google",
+    "Twitter",
+    "Facebook",
+    "etc..."
+  ],
+  "temVariasVantagens": [
+    "Legível",
+    "Fácil de usar",
+    "Leve",
+    "Popular",
+    "Versátil"
+  ],
+  "muitasLinguagensDaoSuporte": [
+    "Python",
+    "C",
+    "C++",
+    "Java",
+    "PHP"
+  ]
+}`;
+
+const usoJSONPorque = JSON.parse(jsonInfo);
+
+const corporationsList = document.getElementById('corporations-used-by');
+const advantagesList = document.getElementById('advantages');
+const languagesList = document.getElementById('languages-used-by');
+
+usoJSONPorque.muitasEmpresasUsam.map((empresa) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = empresa;
+  corporationsList.appendChild(newLi);
+});
+
+usoJSONPorque.temVariasVantagens.map((vantagens) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = vantagens;
+  advantagesList.appendChild(newLi);
+});
+
+usoJSONPorque.muitasLinguagensDaoSuporte.map((linguagens) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = linguagens;
+  languagesList.appendChild(newLi);
+});
+
+// Exemplo de request via terminal: wget 'https://pokeapi.co/api/v2/pokemon/ditto' -O - -q
