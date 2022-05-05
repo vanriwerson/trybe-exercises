f3550547feb139fed562f86e6439f6767bd061fe
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-05 09:35:21 -0300

    Fixação order concluída

--- 02front-end/bloco-10-introducao-a-react/dia-2-componentes-react/conteudo-fixacao/src/App.js
+++ 02front-end/bloco-10-introducao-a-react/dia-2-componentes-react/conteudo-fixacao/src/App.js
@@ -35,21 +35,22 @@ const album02 = {
 
 const joao = {
   id: 102,
-  name: "João",
-  email: "joao@gmail.com",
-  avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+  name: 'João',
+  email: 'joao@gmail.com',
+  avatar:
+    'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png'
 };
 
 const amelia = {
   id: 77,
-  name: "Amélia",
-  email: "amelia@gmail.com",
-  avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+  name: 'Amélia',
+  email: 'amelia@gmail.com',
+  avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png'
 };
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <UserProfile user={joao} />
       <UserProfile user={amelia} />
       {/* <Album album={ album01 } />
