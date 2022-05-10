1a3ec4d224df04a169ab8624b5962f77afec148e
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-10 10:59:16 -0300

    Alteração do estado através de clique concluída

--- 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -4,30 +4,45 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
+    
+    this.state = {
+      helloClicks: 0,
+      conversationClicks: 0,
+      goodbyeClicks: 0
+    }
+    
     this.hello = this.hello.bind(this)
     this.conversation = this.conversation.bind(this)
     this.goodbye = this.goodbye.bind(this)
   }
   
-  hello() {
-    console.log(this);
+  hello = () => {
     console.log('Hello!');
+    this.setState((previousState, _props) => ({
+      helloClicks: previousState.helloClicks + 1
+    }));
   }
 
   conversation() {
     console.log("How're doing?");
+    this.setState((previousState, _props) => ({
+      conversationClicks: previousState.conversationClicks + 1
+    }));
   }
 
   goodbye() {
     console.log('Goodbye!');
+    this.setState((previousState, _props) => ({
+      goodbyeClicks: previousState.goodbyeClicks + 1
+    }));
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={this.hello}>Log1</button>
-        <button onClick={this.conversation}>Log2</button>
-        <button onClick={this.goodbye}>Log3</button>
+        <button onClick={this.hello}>{this.state.helloClicks}</button>
+        <button onClick={this.conversation}>{this.state.conversationClicks}</button>
+        <button onClick={this.goodbye}>{this.state.goodbyeClicks}</button>
       </div>
     );
   }
