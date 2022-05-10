df5551a71911991d40973e5c4f0b37e1477ba443
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-10 09:53:18 -0300

    Refatoração de fancy-buttons com this e bind

--- 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.hello = this.hello.bind(this)
+    this.conversation = this.conversation.bind(this)
+    this.goodbye = this.goodbye.bind(this)
+  }
+  
   hello() {
+    console.log(this);
     console.log('Hello!');
   }
 
