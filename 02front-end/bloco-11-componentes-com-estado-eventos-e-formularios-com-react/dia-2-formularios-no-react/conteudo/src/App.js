aeff6e79a2ccd38daafad6c6f93b556b6bba6bd6
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-11 13:20:12 -0300

    Refatorando App para componente de classe

--- 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/conteudo/src/App.js
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/conteudo/src/App.js
@@ -1,10 +1,43 @@
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      Teste
-    </div>
-  );
+class App extends Component{
+  state = {
+    cor: 'Vermelho',
+    entendeu: false,
+    nota: 10,
+    observacoes: '',
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Componentes com Estado, Eventos e Formulários com React</h1>
+        <form>
+          <select>
+            <option>Amarelo</option>
+            <option>Azul</option>
+            <option selected>Vermelho</option>
+          </select>
+  
+          <label>
+            <input name="entendeu" type="checkbox"></input>
+            Entendeu a matéria de hoje?
+          </label>
+  
+          <label>
+            Que nota esse form merece?
+            <input name="nota" type="number"></input>
+          </label>
+  
+          <label>
+            Justifique sua nota:
+            <textarea name="observacoes" />
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
