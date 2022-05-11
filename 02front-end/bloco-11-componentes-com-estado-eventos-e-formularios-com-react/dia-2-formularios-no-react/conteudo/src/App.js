84525f9f2047ef4f161780ca747446f281e8b775
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-11 17:21:47 -0300

    Finalizando controle dos componentes

--- 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/conteudo/src/App.js
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/conteudo/src/App.js
@@ -1,45 +1,60 @@
 import React, { Component } from 'react';
 import './App.css'
+import Checkbox from './components/Checkbox';
+import Select from './components/Select';
 
 class App extends Component{
   state = {
-    modo: 'React',
+    modo: 'Aos poucos',
     entendeu: false,
     nota: 10,
     observacoes: '',
   }
   
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({
-      name: value,
-    });
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <h1>Componentes com Estado, Eventos e Formulários com React</h1>
         <form>
-          <select name="modo" onChange={(e) => this.handleChange(e)}>
-            <option>Aos poucos</option>
-            <option>aprendo</option>
-            <option>React</option>
-          </select>
-  
-          <label>
-            <input name="entendeu" type="checkbox"></input>
-            Entendeu a matéria de hoje?
-          </label>
+          <Select
+            value={this.state.modo}
+            change={(e) => this.handleChange(e)}
+          />
+          <Checkbox
+            value={this.state.entendeu}
+            change={(e) => this.handleChange(e)}
+          />
   
           <label>
             Que nota esse form merece?
-            <input name="nota" type="number"></input>
+            <input
+              name="nota"
+              type="number"
+              value={this.state.nota}
+              onChange={(e) => this.handleChange(e)}
+            ></input>
           </label>
   
           <label>
             Justifique sua nota:
-            <textarea name="observacoes" />
+            <textarea
+              name="observacoes"
+              value={this.state.observacoes}
+              onChange={(e) => this.handleChange(e)}
+            />
           </label>
+
+          <p>
+            O componente 'input:file' não é controlado pelo React:
+            <input type="file"></input> 
+          </p>
         </form>
       </div>
     );
---
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/conteudo/src/components/Select.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Select extends Component{
+  render() {
+    const { change, value } = this.props;
+    return(
+      <select name="modo" onChange={ change } value={ value }>
+      <option>Aos poucos</option>
+      <option>aprendo</option>
+      <option>React</option>
+    </select>
+    );
+  }
+}
+
+export default Select;
