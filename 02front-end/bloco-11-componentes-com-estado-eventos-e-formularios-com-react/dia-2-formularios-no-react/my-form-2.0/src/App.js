af525d2d8db0de1667bcef13c32b04c37b17d04c
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-12 11:07:27 -0300

    Componentização do select dos Estados

--- 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ 02front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import RequiredTextInput from './components/RequiredTextInput';
+import SelectState from './components/SelectState';
 
 class App extends Component{
   state = {
@@ -66,45 +67,10 @@ class App extends Component{
             value={cidade}
           />
 
-          {/* select com os estados brasileiros: https://gist.github.com/quagliato/9282728 */}
-          <label htmlFor="estado">
-            Estado:
-            <select
-              name="estado"
-              onChange={(e) => this.handleChange(e)}
-              value={this.state.estado}
-              required={true}
-            >
-              <option value="">Selecione</option>
-              <option value="AC">Acre</option>
-              <option value="AL">Alagoas</option>
-              <option value="AP">Amapá</option>
-              <option value="AM">Amazonas</option>
-              <option value="BA">Bahia</option>
-              <option value="CE">Ceará</option>
-              <option value="DF">Distrito Federal</option>
-              <option value="ES">Espírito Santo</option>
-              <option value="GO">Goiás</option>
-              <option value="MA">Maranhão</option>
-              <option value="MT">Mato Grosso</option>
-              <option value="MS">Mato Grosso do Sul</option>
-              <option value="MG">Minas Gerais</option>
-              <option value="PA">Pará</option>
-              <option value="PB">Paraíba</option>
-              <option value="PR">Paraná</option>
-              <option value="PE">Pernambuco</option>
-              <option value="PI">Piauí</option>
-              <option value="RJ">Rio de Janeiro</option>
-              <option value="RN">Rio Grande do Norte</option>
-              <option value="RS">Rio Grande do Sul</option>
-              <option value="RO">Rondônia</option>
-              <option value="RR">Roraima</option>
-              <option value="SC">Santa Catarina</option>
-              <option value="SP">São Paulo</option>
-              <option value="SE">Sergipe</option>
-              <option value="TO">Tocantins</option>
-            </select>
-          </label>
+          <SelectState
+            onChange={(e) => this.handleChange(e)}
+            value={estado}
+          />
         </fieldset>
         <fieldset>
           <legend>Último Emprego</legend>
