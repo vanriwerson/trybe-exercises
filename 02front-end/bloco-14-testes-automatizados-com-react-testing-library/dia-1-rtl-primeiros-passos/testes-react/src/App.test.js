12de9613c7c79a63d5963083ed0d8ebec3edbd27
Bruno Riwerson Silva <riwerson@gmail.com>
2022-06-01 08:56:25 -0300

    Exemplos de testes com simulação de interações do usuário

--- 02front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ 02front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -1,8 +1,43 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verifica se existe o campo Email.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+test('Verifica se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
+
+test('Verifica se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
+});
+
+test("Verifica que, ao digitar o e-mail e clicar em 'Enviar', o e-mail é renderizado", () => {
+  render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
 });
