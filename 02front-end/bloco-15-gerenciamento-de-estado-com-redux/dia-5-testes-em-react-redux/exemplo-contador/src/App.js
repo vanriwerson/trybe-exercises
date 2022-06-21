38aba6f8bf320f27332fc8b86d0c81b1f17fc3e1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-06-21 17:21:32 -0300

    Testando renderização inicial

--- 02front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/exemplo-contador/src/App.js
+++ 02front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/exemplo-contador/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
 import ButtonClicks from './ButtonClicks';
 import NumberClicks from './NumberClicks';
 import './App.css';
@@ -9,10 +7,8 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Provider store={store}>
-          <ButtonClicks />
-          <NumberClicks />
-        </Provider>
+        <ButtonClicks />
+        <NumberClicks />
       </div>
     );
   }
--- 02front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/exemplo-contador/src/App.test.js
+++ 02front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/exemplo-contador/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('The page should have a button and a text "0"', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+  });
 });
