b4ea5c4a00f3129bbde9a6723393f17c965b910b
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-19 15:14:40 -0300

    Adicionando faixas de peso à calculadora

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios22.1/my-scripts/bmi.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios22.1/my-scripts/bmi.js
@@ -8,3 +8,17 @@ const calculateBMI = (weight, height) => weight / (height * height);
 const bmi = calculateBMI(weight, height);
 
 console.log(`Your BMI is ${bmi.toFixed(2)}`);
+
+if (bmi < 18.5) {
+  console.log("Abaixo do peso");
+} else if (bmi >= 18.5 && bmi < 25) {
+  console.log("Peso normal");
+} else if (bmi >= 25 && bmi < 30) {
+  console.log("Sobrepeso");
+} else if (bmi >= 30 && bmi < 35) {
+  console.log("Obesidade grau I");
+} else if (bmi >= 35 && bmi < 40) {
+  console.log("Obesidade grau II");
+} else {
+  console.log("Obesidade mórbida");
+}
