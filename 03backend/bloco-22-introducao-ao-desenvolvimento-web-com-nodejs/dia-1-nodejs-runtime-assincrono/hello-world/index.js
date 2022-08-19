05e903a388a8be6e432259cf2bb5917684b9165f
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-19 09:23:02 -0300

    catching user inputs

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/hello-world/index.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/hello-world/index.js
@@ -1,1 +1,6 @@
-console.log("Hello, Nodejs!");
+const readline = require("readline-sync");
+
+const name = readline.question("What's your name? ");
+const age = readline.questionInt("How old are you? ");
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
