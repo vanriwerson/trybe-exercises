014c2fcb568e79f9ad5aac931888836ac4bcb4bf
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-21 07:53:45 -0300

    Creating server myActivities and endpoint myActivities/:id

---
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/myActivities/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  const selectedActivity = activities.find((activity) => activity.id === Number(id));
+
+  res.status(200).json({ selectedActivity });
+});
+
+module.exports = app;
