0043ef57ad7ae95a632b8efcdd9393403f9dbfa6
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-21 08:32:33 -0300

    Adding filter by status

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/myActivities/src/app.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/myActivities/src/app.js
@@ -23,11 +23,21 @@ const app = express();
 app.use(express.json());
 
 app.get('/myActivities', (req, res) => res.status(200).json({ activities }));
+
 app.get('/myActivities/:id', (req, res) => {
   const { id } = req.params;
   const selectedActivity = activities.find((activity) => activity.id === Number(id));
 
   res.status(200).json({ selectedActivity });
 });
+
+app.get('/filter/myActivities/', (req, res) => {
+  const { status } = req.query;
+  console.log(req.query);
+  const filteredActivities = activities.filter((activity) => activity.status === status);
+
+  res.status(200).json({ filteredActivities });
+});
+// See in the browser with: http://localhost:3001/filter/myActivities/?status=A%20fazer
 
 module.exports = app;
