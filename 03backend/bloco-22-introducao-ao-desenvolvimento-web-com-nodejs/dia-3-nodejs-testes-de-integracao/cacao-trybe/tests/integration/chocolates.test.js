2d263a30c9379ed364057a2c76961f0c6deb4f5e
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-22 08:23:35 -0300

    Adicionando mock para os testes

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,26 +1,35 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const fs = require('fs');
+
 const app = require('../../src/app');
+const mockFile = require('../mocks/mockCacaoTrybeDB');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('Usando o método GET em /chocolates', function () {
+describe('Testando a API Cacao Trybe', function () {
+  sinon.stub(fs.promises, 'readFile')
+  .resolves(mockFile);
+
+  describe('Usando o método GET em /chocolates', function () {
     it('Retorna a lista completa de chocolates!', async function () {
-      const response = await chai
-      .request(app)
-      .get('/chocolates');
-
-      expect(response.status).to.be.equal(200);
-
-      const output =   [
-        { "id": 1, "name": "Mint Intense", "brandId": 1 },
-        { "id": 2, "name": "White Coconut", "brandId": 1 },
-        { "id": 3, "name": "Mon Chéri", "brandId": 2 },
-        { "id": 4, "name": "Mounds", "brandId": 3 }
-      ];
-
-      expect(response.body.chocolates).to.deep.equal(output);
-    });
+        const response = await chai
+        .request(app)
+        .get('/chocolates');
+  
+        expect(response.status).to.be.equal(200);
+  
+        const output =   [
+          { "id": 1, "name": "Mint Intense", "brandId": 1 },
+          { "id": 2, "name": "White Coconut", "brandId": 1 },
+          { "id": 3, "name": "Mon Chéri", "brandId": 2 },
+          { "id": 4, "name": "Mounds", "brandId": 3 }
+        ];
+  
+        expect(response.body.chocolates).to.deep.equal(output);
+      });
+  });
 });
