bc5acef140c253c38c2d1eae676530d2c82086e1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-22 07:54:55 -0300

    Teste para o endpoint /chocolates

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,13 +1,25 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-chai.use(chaiHttp);
-
 const { expect } = chai;
+
+chai.use(chaiHttp);
 
 describe('Usando o método GET em /chocolates', function () {
     it('Retorna a lista completa de chocolates!', async function () {
-        response = await minhaRequisicao();
-        expect(response.status).to.be.equal(200);
+      const response = await chai
+      .request(app)
+      .get('/chocolates');
+
+      expect(response.status).to.be.equal(200);
+
+      const output =   [
+        { "id": 1, "name": "Mint Intense", "brandId": 1 },
+        { "id": 2, "name": "White Coconut", "brandId": 1 },
+        { "id": 3, "name": "Mon Chéri", "brandId": 2 },
+        { "id": 4, "name": "Mounds", "brandId": 3 }
+      ];
+
+      expect(response.body.chocolates).to.deep.equal(output);
     });
 });
