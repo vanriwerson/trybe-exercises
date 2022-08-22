0aa0c2977bb8113c9596bd722f863ed7808a6e7d
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-22 08:36:42 -0300

    Adicionando funções auxiliares

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/src/cacaoTrybe.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/src/cacaoTrybe.js
@@ -1,10 +1,10 @@
-const fs = require('fs').promises;
-const { join } = require('path');
+const fs = require("fs").promises;
+const { join } = require("path");
 
 const readCacaoTrybeFile = async () => {
-  const path = '/files/cacaoTrybeFile.json';
+  const path = "/files/cacaoTrybeFile.json";
   try {
-    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    const contentFile = await fs.readFile(join(__dirname, path), "utf-8");
     return JSON.parse(contentFile);
   } catch (error) {
     return null;
@@ -16,6 +16,20 @@ const getAllChocolates = async () => {
   return cacaoTrybe.chocolates;
 };
 
+const getChocolateById = async (id) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates.filter((chocolate) => chocolate.id === id);
+};
+
+const getChocolatesByBrand = async (brandId) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates.filter(
+    (chocolate) => chocolate.brandId === brandId
+  );
+};
+
 module.exports = {
-    getAllChocolates,
+  getAllChocolates,
+  getChocolateById,
+  getChocolatesByBrand,
 };
