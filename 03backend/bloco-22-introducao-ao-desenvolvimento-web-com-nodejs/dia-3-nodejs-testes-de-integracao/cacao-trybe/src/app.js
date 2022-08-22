e635af1393e241192fb173ae1fb71300009774df
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-22 08:33:06 -0300

    Adicionando as rotas exigidas pelos testes

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/src/app.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/src/app.js
@@ -8,4 +8,16 @@ app.get('/chocolates', async (req, res) => {
   res.status(200).json({ chocolates });
 });
 
+app.get('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+  const chocolate = await cacaoTrybe.getChocolateById(Number(id));
+  res.status(200).json({ chocolate });
+});
+
+app.get('/chocolates/brand/:brandId', async (req, res) => {
+  const { brandId } = req.params;
+  const chocolates = await cacaoTrybe.getChocolatesByBrand(Number(brandId));
+  res.status(200).json({ chocolates });
+});
+
 module.exports = app;
