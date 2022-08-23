87a0d0ee93b7afe9166b45a59d27e2a48aee9671
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-23 16:01:18 -0300

    Geting activities

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/ecoturismo/src/app.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/ecoturismo/src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const activities = require('./files/activities.json');
 
 const app = express();
 app.use(express.json());
 
+app.get('/activities', async (req, res) => {
+  res.status(200).json({ activities });
+});
+
 app.post('/activities', (req, res) => {
-  const activity = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
+  const activity = { ...req.body };
+  activities.push(activity);
+  res.status(201).json({ message: 'Atividade registrada com sucesso!' });
 });
 
 module.exports = app;
