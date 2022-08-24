9c06c7fe29cd24c79ffb725c6a28c459552a4763
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-24 14:41:11 -0300

    installing testing tools

--- 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-nodejs-express-e-mysql/trybecash-api/src/server.js
+++ 03backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-nodejs-express-e-mysql/trybecash-api/src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const connection = require('./db/connection');
+// const connection = require('./db/connection');
 
 const port = 3001;
 
@@ -7,9 +7,8 @@ app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
-  const [ result ] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
-
+  // const [ result ] = await connection.execute('SELECT 1');
+  // if (result) {
+  //   console.log('MySQL connection OK');
+  // }
 });
