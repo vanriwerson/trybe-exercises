fe0e90f773297f33ad43a19af53f2e03ccbe3593
Bruno Riwerson Silva <riwerson@gmail.com>
2022-09-15 18:22:48 -0300

    Exemplo subiu

---
+++ 03backend/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,10 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
