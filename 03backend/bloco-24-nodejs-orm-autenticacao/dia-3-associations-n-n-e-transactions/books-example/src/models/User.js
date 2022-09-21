3b930260e6e46a33a12c368a85f3e5a838ae6455
Bruno Riwerson Silva <riwerson@gmail.com>
2022-09-21 08:28:01 -0300

    config: banco de dados e associations N:N

---
+++ 03backend/bloco-24-nodejs-orm-autenticacao/dia-3-associations-n-n-e-transactions/books-example/src/models/User.js
@@ -0,0 +1,15 @@
+// src/models/User.js
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    userId: { type: DataTypes.INTEGER, primaryKey: true },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  },
+    {
+      timestamps: false,
+      underscored: true,
+    });
+
+  return User;
+};
