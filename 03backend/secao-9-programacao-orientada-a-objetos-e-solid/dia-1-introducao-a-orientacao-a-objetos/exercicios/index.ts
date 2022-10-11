55e3e491213999d7d439f78001ea5a9304b6a40a
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-11 10:12:35 -0300

    exercise date

--- 03backend/secao-9-programacao-orientada-a-objetos-e-solid/dia-1-introducao-a-orientacao-a-objetos/exercicios/index.ts
+++ 03backend/secao-9-programacao-orientada-a-objetos-e-solid/dia-1-introducao-a-orientacao-a-objetos/exercicios/index.ts
@@ -1,5 +1,6 @@
 import Order from "./Order";
 import Student from "./Student";
+import Data from "./Date";
 
 const student = new Student('01ADS11102022', 'Bruno', [5, 6, 7, 8], [9, 10]);
 console.log(student.getGradesSum());
@@ -8,3 +9,34 @@ console.log(student.getAverage());
 const order = new Order('Bruno', [{item: 'coxinha', price: 5}, {item: 'suco', price: 5}], 'dinheiro', 0.1);
 console.log(order.getTotalConsumption());
 console.log(order.getTotalConsumptionWithDiscount());
+
+const testDate = new Data(29, 1, 1989);
+  
+console.log(testDate);
+console.log('Dia: ', testDate.day);
+console.log('Mês: ', testDate.month);
+console.log('Mês por extenso: ', testDate.getMonthName());
+console.log('Ano: ', testDate.year);
+console.log('É ano bissexto: ', testDate.isLeapYear() ? 'Sim' : 'Não');
+console.log(testDate.format('dd/mm/aaaa'));
+console.log(testDate.format('dd-mm-aaaa'));
+console.log(testDate.format('aaaa/mm/dd'));
+console.log(testDate.format('aaaa-mm-dd'));
+console.log(testDate.format('dd de M de aa'));
+console.log(testDate.format('dd, M de aaaa'));
+
+const otherDate = new Data(30, 1, 2021);
+
+const compared = testDate.compare(otherDate);
+
+const compareStates = ['anterior', 'igual', 'posterior'];
+
+console.log(`A primeira data é ${compareStates[compared + 1]} a segunda.`);
+
+// data inválida
+const invalidDate = new Data(31, 2, 2021);
+
+console.log('Teste data inválida: ', invalidDate);
+
+// formato inválido
+console.log(invalidDate.format('a m d'));
