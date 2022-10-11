ca36031fcc0d65ef617f6572fbeded543a7ac5dc
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-11 08:14:16 -0300

    example: mail sender

---
+++ 03backend/secao-9-programacao-orientada-a-objetos-e-solid/dia-1-introducao-a-orientacao-a-objetos/exemplo/MailList.ts
@@ -0,0 +1,29 @@
+import Email from "./Email";
+
+export default class MailList {
+  // Essa sintaxe no construtor é chamada `Parameter Properties`
+  // É um atalho para declarar e automaticamente atribuir o valor que será recebido via parâmetro ao atributo privado `mailList`
+  constructor(private mailList: Email[] = []) { }
+
+  get all(): Email[] { return this.mailList; }
+
+  getEmailsSentBy(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.from === mailAddress);
+  }
+
+  getEmailsTo(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.to === mailAddress);
+  }
+
+  getEmailsBySubject(searchString: string): Email[] {
+    return this.mailList
+      .filter((mail) => mail.subject.indexOf(searchString) !== -1);
+  }
+
+  addEmail(newMail: Email): void { this.mailList.push(newMail); }
+
+  removeEmail(mailToRemove: Email): void {
+    // note que como essa é uma comparação de objetos, esse filter só funcionará se a referência de `mail` e `mailToRemove` for a mesma
+    this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
+  }
+}
