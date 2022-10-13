963a9eb9da3f68bf8285c2c4edce943b406312fd
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-13 14:20:02 -0300

    finalizando exercícios

--- 03backend/secao-9-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/object-oriented-school/index.ts
+++ 03backend/secao-9-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/object-oriented-school/index.ts
@@ -1,6 +1,8 @@
 import Person from './Person';
 import Student from "./Student";
 import IEmployee from './Employee';
+import Subject from './Subject';
+import Teacher from './Teacher';
 
 const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
 const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
@@ -31,3 +33,18 @@ const employee: IEmployee = {
 
 console.log(employee);
 
+const math = new Subject('Matemática');
+const history = new Subject('História');
+const english = new Subject('Inglês');
+
+console.log(math);
+console.log(history);
+console.log(english);
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, english);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
