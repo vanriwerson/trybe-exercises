f77fdf05bad6b9da003829ef4f84fab417af53da
Bruno Riwerson Silva <riwerson@gmail.com>
2023-01-21 09:05:34 -0300

    exemplos de operações básicas

---
+++ 04ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/exemplos.py
@@ -0,0 +1,8 @@
+print(2 * 3)  # saída: 6
+print(2 + 3)  # saída: 5
+print(3 / 2)  # saída: 1.5
+print(3 // 2)  # divisão inteira (arredondamento para baixo) - saída: 1
+print(3 ** 2) # potência - saída: 9
+
+sqrt = 25 ** (1/2)
+print(sqrt)
